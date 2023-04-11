3f17e4f1481aa811ccf3531379abfa7b0ebd2dbb
thomascountz <thomascountz@gmail.com>
2023-04-11 11:49:15 +0200

    default deploy to /bench/

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: process.env.NODE_ENV === 'production'
-  ? '/bench/'
-  : '/'
+  base: '/bench/',
 })
